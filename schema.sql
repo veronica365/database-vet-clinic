8547d8107712e23b87d60f2dbd7ce470f6c93ded
veronica365 <naruth10@gmail.com>
2023-05-02 19:26:39 +0300

    add functionality to create animal table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
 /* Database schema to keep the structure of entire database. */
-
+DROP SEQUENCE IF EXISTS animals_id_seq CASCADE;
+CREATE SEQUENCE animals_id_seq;
+DROP TABLE IF EXISTS animals;
 CREATE TABLE animals (
-    name varchar(100)
+    id integer NOT NULL DEFAULT nextval('animals_id_seq'),
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg decimal(5,2)
 );
+
+ALTER SEQUENCE animals_id_seq
+OWNED BY animals.id;
