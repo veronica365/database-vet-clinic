f5bc60fc368a2e3ca3d1430a665e8bb07a908a2b
veronica365 <naruth10@gmail.com>
2023-05-02 19:34:07 +0300

    Add sql for adding data to the database

--- data.sql
+++ data.sql
@@ -1,5 +1,8 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES
+('Augumon', '02-03-2020', 0, true, 10.23),
+('Gabumon', '11-15-2018', 0, true, 8),
+('Pikachu', '01-07-2021', 0, true, 15.04),
+('Devimon', '05-12-2020', 0, true, 11);
