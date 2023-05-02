c319cc5e21bc104450a9fcb63784e343cb30420c
veronica365 <naruth10@gmail.com>
2023-05-02 20:00:13 +0300

    Add functionality to run all the required queries

--- data.sql
+++ data.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES
-('Augumon', '02-03-2020', 0, true, 10.23),
-('Gabumon', '11-15-2018', 0, true, 8),
-('Pikachu', '01-07-2021', 0, true, 15.04),
-('Devimon', '05-12-2020', 0, true, 11);
+('Agumon', '02-03-2020', 0, true, 10.23),
+('Gabumon', '11-15-2018', 2, true, 8),
+('Pikachu', '01-07-2021', 1, false, 15.04),
+('Devimon', '05-12-2020', 5, true, 11);
