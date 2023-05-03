b7d23f91e8e40f13cdb2d7fd35daffe7b112dad6
veronica365 <naruth10@gmail.com>
2023-05-03 14:07:26 +0300

    Add SQL to insert more data into the animals column

--- data.sql
+++ data.sql
@@ -6,3 +6,13 @@ VALUES
 ('Gabumon', '11-15-2018', 2, true, 8),
 ('Pikachu', '01-07-2021', 1, false, 15.04),
 ('Devimon', '05-12-2020', 5, true, 11);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES
+('Charmander', '02-08-2020', 0, false, 11),
+('Plantmon', '11-15-2021', 2, true, -5.7),
+('Squirtle', '04-02-1993', 3, false, -12.13),
+('Angemon', '06-12-2005', 1, true, -45),
+('Boarmon', '06-07-2005', 7, true, 20.4),
+('Blossom', '10-13-1998', 3, true, 17),
+('Ditto', '05-14-2022', 4, true, 22);
