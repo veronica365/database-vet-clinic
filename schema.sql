f461b2f5317e0d5b140eaef713d5598669099e5c
veronica365 <naruth10@gmail.com>
2023-05-05 14:02:38 +0300

    Add functionality to modify tables

--- schema.sql
+++ schema.sql
@@ -10,9 +10,32 @@ CREATE TABLE animals (
     neutered boolean,
     weight_kg decimal(5,2)
 );
-
-ALTER SEQUENCE animals_id_seq
-OWNED BY animals.id;
-
+ALTER SEQUENCE animals_id_seq OWNED BY animals.id;
 ALTER TABLE animals DROP COLUMN IF EXISTS species;
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+DROP SEQUENCE IF EXISTS owners_id_seq CASCADE;
+CREATE SEQUENCE owners_id_seq;
+DROP TABLE IF EXISTS owners;
+CREATE TABLE owners (
+    id integer NOT NULL DEFAULT nextval('owners_id_seq') PRIMARY KEY,
+    fullname varchar(255),
+    age integer
+);
+ALTER SEQUENCE owners_id_seq OWNED BY owners.id;
+
+DROP SEQUENCE IF EXISTS species_id_seq CASCADE;
+CREATE SEQUENCE species_id_seq;
+DROP TABLE IF EXISTS species;
+CREATE TABLE species (
+    id integer NOT NULL DEFAULT nextval('species_id_seq') PRIMARY KEY,
+    name varchar(255)
+);
+ALTER SEQUENCE species_id_seq OWNED BY species.id;
+
+ALTER TABLE animals ADD CONSTRAINT animals_pk PRIMARY KEY (id);
+ALTER TABLE animals DROP COLUMN IF EXISTS species;
+ALTER TABLE animals DROP COLUMN IF EXISTS species_id;
+ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
+ALTER TABLE animals DROP COLUMN IF EXISTS owner_id;
+ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
