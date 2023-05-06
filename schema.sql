20cffcdacb766ad33f5b83669755139c108cd2a5
veronica365 <naruth10@gmail.com>
2023-05-06 20:11:50 +0300

    Add functionality to create tables

--- schema.sql
+++ schema.sql
@@ -14,6 +14,7 @@ ALTER SEQUENCE animals_id_seq OWNED BY animals.id;
 ALTER TABLE animals DROP COLUMN IF EXISTS species;
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
 
+
 DROP SEQUENCE IF EXISTS owners_id_seq CASCADE;
 CREATE SEQUENCE owners_id_seq;
 DROP TABLE IF EXISTS owners;
@@ -23,6 +24,7 @@ CREATE TABLE owners (
     age integer
 );
 ALTER SEQUENCE owners_id_seq OWNED BY owners.id;
+
 
 DROP SEQUENCE IF EXISTS species_id_seq CASCADE;
 CREATE SEQUENCE species_id_seq;
@@ -39,3 +41,36 @@ ALTER TABLE animals DROP COLUMN IF EXISTS species_id;
 ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
 ALTER TABLE animals DROP COLUMN IF EXISTS owner_id;
 ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+
+
+DROP SEQUENCE IF EXISTS vets_id_seq CASCADE;
+CREATE SEQUENCE vets_id_seq;
+DROP TABLE IF EXISTS vets;
+CREATE TABLE vets (
+    id integer NOT NULL DEFAULT nextval('vets_id_seq') PRIMARY KEY,
+    name VARCHAR(255),
+    age integer,
+    date_of_graduation date
+);
+ALTER SEQUENCE vets_id_seq OWNED BY vets.id;
+
+DROP SEQUENCE IF EXISTS specializations_id_seq CASCADE;
+CREATE SEQUENCE specializations_id_seq;
+DROP TABLE IF EXISTS specializations;
+CREATE TABLE specializations (
+    id integer NOT NULL DEFAULT nextval('specializations_id_seq') PRIMARY KEY,
+    vet_id integer REFERENCES vets(id),
+    species_id integer REFERENCES species(id)
+);
+ALTER SEQUENCE specializations_id_seq OWNED BY specializations.id;
+
+DROP SEQUENCE IF EXISTS visits_id_seq CASCADE;
+CREATE SEQUENCE visits_id_seq;
+DROP TABLE IF EXISTS visits;
+CREATE TABLE visits (
+    id integer NOT NULL DEFAULT nextval('visits_id_seq') PRIMARY KEY,
+    animal_id integer REFERENCES animals(id),
+    vet_id integer REFERENCES vets(id),
+    date_of_visit date
+);
+ALTER SEQUENCE visits_id_seq OWNED BY visits.id;
